a9a0d577671b1b02ddd9af1bad54834b489c91df
Adam Bachman <adam.bachman@gmail.com>
2024-03-30 12:53:13 -0400

    bundle gem csv_author

---
+++ lib/csv_author.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "csv_author/version"
+
+module CsvAuthor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/csv_author_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CsvAuthor do
+  it "has a version number" do
+    expect(CsvAuthor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
